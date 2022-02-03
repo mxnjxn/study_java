96c0cf679a84bb9ce17233a6cc9c4bf5d39e08d2
mxnjxn <choe86898@gmail.com>
2022-02-03 16:41:47 +0900

    .

--- src/com/naver/lecture/cho2/Hello.java
+++ src/com/naver/lecture/cho2/Hello.java
@@ -5,6 +5,13 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Java worlsdfasfd.");
+		
+		int a =1;
+		int b =2;
+		int c = 0;
+		
+		c= a + b;
+		System.out.println("c");
 	}
 
 }
