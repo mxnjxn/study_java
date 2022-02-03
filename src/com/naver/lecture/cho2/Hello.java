0b2ba11c3ccc2e78d9e56f9d24065763250cbc51
mxnjxn <choe86898@gmail.com>
2022-02-03 17:12:31 +0900

    .

--- src/com/naver/lecture/cho2/Hello.java
+++ src/com/naver/lecture/cho2/Hello.java
@@ -6,12 +6,13 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to the Java worlsdfasfd.");
 		
-		int a =1;
-		int b =2;
+		int a =1;  //선언 --> 선언 만 해서는 메모리에 할당이 되지 않음. 
+		int b =2;  //초기화 
 		int c = 0;
+		// String str =""
 		
 		c= a + b;
-		System.out.println("c");
+		System.out.println(c);
 	}
 
 }
