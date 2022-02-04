2fb2623c1ffe5ab0fa00c149d65c627b3ccf8771
mxnjxn <choe86898@gmail.com>
2022-02-04 14:29:56 +0900

    ..

---
+++ src/com/naver/lecture/cho2/Variable.java
@@ -0,0 +1,21 @@
+package com.naver.lecture.cho2;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		// 변수 명명 법
+		// 첫글자 소문자, 다음 문자열 부터는 대문자
+		// 모든 변수명은 띄어쓰기 없이 붙여서
+		
+		
+		int roomNumber = 1; // 두 변수는 서로 다름 변수임 
+		// 대소문자 구분함
+		int roomnumber =2;
+		
+		System.out.println("roomNumber:" + roomNumber);
+		System.out.println("roomnumber:" + roomnumber);
+		
+	
+	}
+
+}
