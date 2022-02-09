59a0f0e4b0e405d3b12dcc6f20290d9ac2edb855
mxnjxn <choe86898@gmail.com>
2022-02-09 17:42:09 +0900

    ..

---
+++ src/com/naver/lecture/cho3/DengLongicOperator.java
@@ -0,0 +1,23 @@
+package com.naver.lecture.cho3;
+
+public class DengLongicOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		boolean play = true;
+		boolean play1 = false;
+		
+		System.out.println(play);
+		
+		play = !play; 
+		System.out.println(play);
+		
+		play = !play; // 논리 부정 연산자 !
+		System.out.println(play);
+		
+		
+		
+	}
+
+}
