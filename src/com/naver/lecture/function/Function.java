b35303a2fa50ffc2e8170717bca783a631028e57
mxnjxn <choe86898@gmail.com>
2022-02-17 16:00:12 +0900

    .

--- src/com/naver/lecture/function/Function.java
+++ src/com/naver/lecture/function/Function.java
@@ -18,6 +18,7 @@ public class Function {
 		
 		double devide = devide(22,4);
 		System.out.println(devide);
+		
 	}
 	
 	//함수 선언 // 내용 밑에 선언. 
@@ -39,13 +40,42 @@ public class Function {
 	}
 	
 	//곱하기
-	public static int multi (int a, int b) {
+	public static int multi (int a, int b) { 
 		int multi = a*b;
 		return multi;
 	}
 	
-	public static double devide (int a, int b) {
+	public static double devide (int a, int b) { //return 형이 int 면 return= 1; 놓고 시작.  // output 데이타형을 public static "____" 함수 이름 (변수, 변수) 에 넣어 준다. 
 		double devide = a/b;
 		return devide; 
 	}
+	
+	// 두 문자열을 합치는 함수.
+	public static String add (String a, String b) {
+		String add = a + b;
+		return add;
+		// string 이면 return: "" 찍고 시작. 
+	}
+	
+	//오늘 뭐 먹지 프로그램
+	//배열 사용
+	//함수 사용
+	// 출력을 위한 함수도 별도로 제직
+	
+	//중식, 한식, 양식, 일식
+	//output: 오늘 추천 드리는 메뉴는 ?? 입니다. 
+	
+	public static String[][] menu (String a,  String b) {
+		String[][] menu = {{"중식", "짜장면"}, {"한식", "비빔면"}, {"일식", "라면"}, {"양식", "스테이크"}};
+		return menu;
+	}
+	
+	public static void println(String str) {
+		if (str == "중식") {
+				System.out.println( menu[1][1]);
+		}
+	}
+	
+	
+	public static dict[:] menu ()
 }
